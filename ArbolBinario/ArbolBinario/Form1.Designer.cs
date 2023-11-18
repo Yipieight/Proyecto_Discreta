73d1abdf9c2baa37949d274bdb869cff757a8164
Jose Garcia <57154305+Yipieight@users.noreply.github.com>
2023-11-17 20:28:38 -0600

    ar

--- ArbolBinario/ArbolBinario/Form1.Designer.cs
+++ ArbolBinario/ArbolBinario/Form1.Designer.cs
@@ -34,10 +34,11 @@
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblNombreNodo = new System.Windows.Forms.Label();
+            this.txtRecorrido = new System.Windows.Forms.TextBox();
             this.lblAltura = new System.Windows.Forms.Label();
             this.lblAncho = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
             this.SuspendLayout();
             // 
             // button1
@@ -48,6 +49,7 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Agregar Raiz";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -57,6 +59,7 @@
             this.button2.TabIndex = 1;
             this.button2.Text = "Agregar Derecha";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
@@ -75,6 +78,7 @@
             this.button4.TabIndex = 3;
             this.button4.Text = "Recorrer InOrden";
             this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button5
             // 
@@ -84,6 +88,7 @@
             this.button5.TabIndex = 4;
             this.button5.Text = "Recorrer PreoOrden";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // button6
             // 
@@ -93,24 +98,25 @@
             this.button6.TabIndex = 5;
             this.button6.Text = "Recorrer PostOrden";
             this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
-            // label1
+            // lblNombreNodo
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(22, 310);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(196, 25);
-            this.label1.TabIndex = 6;
-            this.label1.Text = "Nodo seleccionado";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.lblNombreNodo.AutoSize = true;
+            this.lblNombreNodo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreNodo.Location = new System.Drawing.Point(22, 315);
+            this.lblNombreNodo.Name = "lblNombreNodo";
+            this.lblNombreNodo.Size = new System.Drawing.Size(196, 25);
+            this.lblNombreNodo.TabIndex = 6;
+            this.lblNombreNodo.Text = "Nodo seleccionado";
+            this.lblNombreNodo.Click += new System.EventHandler(this.label1_Click);
             // 
-            // textBox1
+            // txtRecorrido
             // 
-            this.textBox1.Location = new System.Drawing.Point(461, 315);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(231, 20);
-            this.textBox1.TabIndex = 7;
+            this.txtRecorrido.Location = new System.Drawing.Point(461, 315);
+            this.txtRecorrido.Name = "txtRecorrido";
+            this.txtRecorrido.Size = new System.Drawing.Size(231, 20);
+            this.txtRecorrido.TabIndex = 7;
             // 
             // lblAltura
             // 
@@ -132,15 +138,23 @@
             this.lblAncho.TabIndex = 9;
             this.lblAncho.Text = "Ancho";
             // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(27, 26);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(413, 276);
+            this.treeView1.TabIndex = 10;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.treeView1);
             this.Controls.Add(this.lblAncho);
             this.Controls.Add(this.lblAltura);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRecorrido);
+            this.Controls.Add(this.lblNombreNodo);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
@@ -162,9 +176,10 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblNombreNodo;
+        private System.Windows.Forms.TextBox txtRecorrido;
         private System.Windows.Forms.Label lblAltura;
         private System.Windows.Forms.Label lblAncho;
+        private System.Windows.Forms.TreeView treeView1;
     }
 }
--- ArbolBinario/ArbolBinario/Form1.cs
+++ ArbolBinario/ArbolBinario/Form1.cs
@@ -22,7 +22,7 @@ namespace ArbolBinario
 
         Nodo crearNodo()
         {
-            string nombre = Interaction.Inputbox("Ingrese nombre del nodo");
+            string nombre = Interaction.InputBox("Ingrese nombre del nodo");
             return new Nodo(nombre);
         }
 
@@ -30,12 +30,134 @@ namespace ArbolBinario
         {
             this.lblAltura.Text = $"Altura:{Alto(raiz)}";
             int inicio = 0;
-            this.lblAncho.Text = 
+            this.lblAncho.Text = $"Ancho:{Ancho(raiz, ref inicio)}";
+        }
+
+        int Ancho(Nodo n, ref int ancho)
+        {
+            if (n.Derecha == null && n.Izquierda == null)
+                ancho += 1;
+            if (n.Derecha != null) Ancho(n.Derecha, ref ancho);
+            if (n.Izquierda != null) Ancho(n.Izquierda, ref ancho);
+
+            return ancho;
+        }
+        int Alto(Nodo n)
+        {
+            if (n == null) return 0;
+
+            int izq = Alto(n.Izquierda) + 1;
+            int der = Alto(n.Derecha) + 1;
+            return Math.Max(izq, der);
         }
 
         private void label1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (raiz != null)
+            {
+                DialogResult r = MessageBox.Show("Se eliminará el árbol desea continuar?", "consulta");
+                if (r == DialogResult.Yes)
+                {
+                    raiz = crearNodo();
+                }
+                else
+                {
+                    raiz = crearNodo();
+                }
+                CambiarSeleccion(raiz);
+                LlenarTreeView();
+            }
+        }
+
+        void CambiarSeleccion(Nodo n)
+        {
+            seleccionado = n;
+            this.lblNombreNodo.Text = n.Nombre;
+        }
+
+        public void LlenarTreeView()
+        {
+            treeView1.Nodes.Clear();
+            MostrarNodo(raiz, null, string.Empty);
+            treeView1.ExpandAll();
+            EvaluarArbol();
+        }
+
+        public void MostrarNodo(Nodo n, TreeNode tnpadre, string lado)
+        {
+            if (n == null) return;
+
+            TreeNode nuevo = new TreeNode();
+            if (tnpadre == null && lado == string.Empty)
+            {
+                nuevo.Text = $"{lado} - {n.Nombre}";
+                nuevo.Tag = n;
+
+                tnpadre.Nodes.Add(nuevo);
+
+            }
+
+            if (n.Derecha != null) MostrarNodo(n.Derecha, nuevo, "D");
+            if (n.Izquierda != null) MostrarNodo(n.Izquierda, nuevo, "I");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (seleccionado != null)
+            {
+                seleccionado.Derecha = crearNodo();
+                LlenarTreeView();
+            }
+            else
+                MessageBox.Show("Debe tener algun nodo seleccionado");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.txtRecorrido.Text = string.Empty;
+            RecorridoPreOrden(raiz);
+        }
+
+        void RecorridoPreOrden(Nodo n)
+        {
+            Visitar(n);
+            if (n.Izquierda != null) RecorridoPreOrden(n.Izquierda);
+            if (n.Derecha != null) RecorridoPreOrden(n.Derecha);
+        }
+
+        void RecorridoInreOrden(Nodo n)
+        {
+            if (n.Izquierda != null) RecorridoInreOrden(n.Izquierda);
+            Visitar(n);
+            if (n.Derecha != null) RecorridoInreOrden(n.Derecha);
+        }
+
+        void RecorridoPostreOrden(Nodo n)
+        {
+            if (n.Izquierda != null) RecorridoInreOrden(n.Izquierda);
+            if (n.Derecha != null) RecorridoInreOrden(n.Derecha);
+            Visitar(n);
+        }
+
+        void Visitar(Nodo n)
+        {
+            this.txtRecorrido.Text += "-" + n.Nombre;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            RecorridoInreOrden(raiz);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            RecorridoPostreOrden(raiz);
         }
     }
 }
